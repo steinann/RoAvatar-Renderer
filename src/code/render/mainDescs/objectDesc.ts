c4ceaf8bd25c563e1d250a12c976904fb4f708f3
Mzigi <101520429+steinann@users.noreply.github.com>
2026-09-10 12:38:15 +0200

    Fixed mesh.visible incorrectly overriding material.visible

--- src/code/render/mainDescs/objectDesc.ts
+++ src/code/render/mainDescs/objectDesc.ts
@@ -201,7 +201,7 @@ export class ObjectDesc extends RenderDesc {
             threeMesh.material = threeMaterial
             threeMesh.receiveShadow = true
             threeMaterial.needsUpdate = true
-            threeMesh.visible = threeMaterial.visible
+            //threeMesh.visible = threeMaterial.visible
 
             this.results = [threeMesh]
             
